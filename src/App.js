324f4fb4d26ad50a9d83bdefd9eaf4fda438f26c
unknown <tobiburrohman2@gmail.com>
2022-07-14 17:02:51 +0600

    daisyui and tailwind is working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
 // demo 1 https://flexiblog-education.netlify.app/
+// demo 2 https://atixscripts.info/demo/html/minimag/index-13.html#
 
 function App() {
   return (
     <>
-      Hello world
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <button class="btn">Button</button>
+<button class="btn btn-primary">Button</button>
+<button class="btn btn-secondary">Button</button>
+<button class="btn btn-accent">Button</button>
+<button class="btn btn-ghost">Button</button>
+<button class="btn btn-link">Button</button>
     </>
   );
 }
