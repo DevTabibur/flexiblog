a6d62efdaf246f5b13cb06c44b54fc8795f3690f
unknown <tobiburrohman2@gmail.com>
2022-07-26 13:12:10 +0600

    firebase info set with environment vareiables

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -5,14 +5,26 @@ import {getAuth} from 'firebase/auth';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBMaploUPCJL4BAbWZK0P7S7jsWSJ1ImVA",
-  authDomain: "flexiblog-topu.firebaseapp.com",
-  projectId: "flexiblog-topu",
-  storageBucket: "flexiblog-topu.appspot.com",
-  messagingSenderId: "176693561238",
-  appId: "1:176693561238:web:9ff9aedca683d897ae3fb7"
+  apiKey:process.env.REACT_APP_API_KEY,
+  authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_APPID,
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBMaploUPCJL4BAbWZK0P7S7jsWSJ1ImVA",
+//   authDomain: "flexiblog-topu.firebaseapp.com",
+//   projectId: "flexiblog-topu",
+//   storageBucket: "flexiblog-topu.appspot.com",
+//   messagingSenderId: "176693561238",
+//   appId: "1:176693561238:web:9ff9aedca683d897ae3fb7"
+// };
+
+console.log(firebaseConfig);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
